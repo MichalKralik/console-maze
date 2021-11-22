480ebcf048748bd0abfc06dc3d7784f94df81e78
Michal Králík <michal.kralik04@gmail.com>
2021-11-22 13:59:20 +0100

    finnish

--- MazeGenerator/Program.cs
+++ MazeGenerator/Program.cs
@@ -6,7 +6,28 @@ namespace MazeGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var mazeSize = 11;
+            var maze = new string[mazeSize, mazeSize];
+
+            for (int i = 0; i < maze.GetLength(0); i++) 
+            {
+                for (int j = 0; j < maze.GetLength(1); j++) 
+                {
+                    maze[i, j] = "X";
+                }
+
+            }
+
+            for (int i = 0; i < maze.GetLength(0); i++) 
+            {
+                for (int j = 0; j < maze.GetLength(1); j++)
+                {
+                    var item = maze[i, j];
+                    Console.Write(item + " ");
+                }
+                Console.Write(Environment.NewLine);
+            }
+
         }
     }
 }
